6c94739e13dce8c0ac98a513bf2c2304050ca2b9
DanAmyakar <dan.rockit@gmail.com>
2020-07-27 22:40:31 -0400

    Updated ships migration from connex slot to cargo slots

--- db/migrate/20200724195547_create_ships.rb
+++ db/migrate/20200724195547_create_ships.rb
@@ -2,7 +2,7 @@ class CreateShips < ActiveRecord::Migration
   def change
     create_table :ships do |t|
       t.string :name
-      t.integer :connex_slots
+      t.integer :cargo_slots
       t.integer :captain_id
     end
   end
