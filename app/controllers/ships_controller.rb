1291e5861b3d16a5db6dbeded103a55950954989
DanAmyakar <dan.rockit@gmail.com>
2020-08-02 19:16:09 -0400

    WIP - added loggin verification to ships controller actions and added delete function to ships show erb file and controller

--- app/controllers/ships_controller.rb
+++ app/controllers/ships_controller.rb
@@ -1,10 +1,12 @@
 class ShipsController < ApplicationController
 
+    # gets to new ship form
     get '/ships/new' do
         redirect_if_not_signed_in
         erb :'ships/new'
     end
 
+    # creates a new ship from form info
     post '/ships' do
         redirect_if_not_signed_in
         if Ship.valid_params?(params)
@@ -16,20 +18,22 @@ class ShipsController < ApplicationController
         end
     end
     
-    #edit method for a ship
-    #edit form contains item lines equal to the cargo slots of a ship
-    # submit writes all the item lines to the ships page
+    # gets the form to write new info to a ship
     get '/captains/ships/:id/edit' do
+        redirect_if_not_signed_in
         @ship_to_edit = Ship.find(params[:id])
         erb :'/ships/edit'
     end
 
+    # writes new info for a ship
     patch '/captains/ships/:id' do
+        redirect_if_not_signed_in
         Ship.update(params[:id], ship_name: params[:ship_name], cargo_slots: params[:cargo_slots])
         @ship = Ship.find(params[:id])
         erb :'ships/show'
     end
 
+    # gets a ships info page
     get '/captains/ships/:id' do
         redirect_if_not_signed_in
         @ship = Ship.find(params["id"])
@@ -37,5 +41,9 @@ class ShipsController < ApplicationController
     end
     
     #detroy method for a ship
-    
+    delete '/captains/ships/:id' do
+        redirect_if_not_signed_in
+        Ship.delete(params[:id])
+        redirect '/captains/ships'
+    end
 end
