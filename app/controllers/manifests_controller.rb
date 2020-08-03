15c917a6f42ab778b6d6273e36ee88c401f489a5
DanAmyakar <dan.rockit@gmail.com>
2020-08-02 20:24:45 -0400

    Added manifests to the db, worte a method from ships/show to manifests/new, put 'unavailable at this time' message in place of form, added content to readme.md

--- app/controllers/manifests_controller.rb
+++ app/controllers/manifests_controller.rb
@@ -1,11 +1,18 @@
 class ManifestsController < ApplicationController
 
-    get "/manifest/new" do 
-        erb :'manifests/new.erb'
+    # gets the form for a new manifest
+    get '/captains/ships/manifests/new' do 
+        redirect_if_not_signed_in
+        "This option is temporarily unavailable"
+        #erb :'manifests/new'
     end
 
-    post "ships/:id" do
-
+    # creates a new manifest from form data and stores it in the ship
+    post "/captains/ships/:id" do
+        redirect_if_not_signed_in
+        @manifest = Manifest.create(params)
+        Ship.find(params["id"]).manifests << @manifest
+        redirect '/captains/ships/:id'
     end
 
 end
