6a27cf8f45bba43513e81c2f7dc0dbc11d91c2f7
DanAmyakar <dan.rockit@gmail.com>
2020-08-13 16:00:30 -0400

    WIP: changed method for creating a captain and siging in

--- app/controllers/captains_controller.rb
+++ app/controllers/captains_controller.rb
@@ -6,42 +6,41 @@ class CaptainsController < ApplicationController
         erb :'captains/create_account'
     end
 
-    post '/captains' do
-        if params[:captain_name] != "" && params[:password] != ""
-            @captain.save if @captain.valid
-            redirect '/captains/#{@captain.id}'
+    post '/captains' do 
+        @captain = Captain.new
+        @captain.save
+        session[:captain_id] = @captain.id    
+        if session[:captain_id] != nil
+            redirect "/captains/#{@captain.id}"
         else
-            redirect '/captains/create_account'
+            redirect '/create_account'
         end
     end
-
-    
 
     get '/sign_in' do
         erb :'captains/sign_in'
     end
 
-    post '/sign' do
+    post '/sign_in' do
         captain = Captain.find_by(captain_name: params[:captain_name])
         if captain && captain.authenticate(params[:password])
             session[:captain_id] = captain.id
-            redirect '/captains/:id'
+            redirect "/captains/#{captain.id}"
         else
             redirect '/sign_in'
         end
     end
-
 
     get '/captains/:id' do
         if signed_in?
             @captain = current_captain
             erb :'/captains/show'
         else
-            redirect to '/sign_out'
+            redirect to '/sign_in'
         end
     end
 
-    get '/sign_out' do
+    delete '/sign_out' do
         !session[:captain_id].nil?
         session.destroy
         redirect '/'
