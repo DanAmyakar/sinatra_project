df632091f24be1c03293cdb154baa27947f2bebd
DanAmyakar <dan.rockit@gmail.com>
2020-08-14 12:58:02 -0400

    WIP: Able to pesist data to manifests page but keep having four values instead of 3. Rewrote manifests/new form and and manifests controller methods to try to use persistant data. Keep running into error for 'Ununiquess' of an attribute.

--- app/controllers/manifests_controller.rb
+++ app/controllers/manifests_controller.rb
@@ -3,14 +3,16 @@ class ManifestsController < ApplicationController
     # gets the form for a new manifest
     get '/manifests/new' do 
         redirect_if_not_signed_in
-        binding.pry
+        @ship = current_captain.ships.find_by_id(params[:ship_id])
+        #binding.pry
         erb :'manifests/new'
     end
 
     # creates a new manifest from form data and stores it in the ship
     post "/ships/:id" do
         redirect_if_not_signed_in
-        @manifest = @current_ship.manifests.new(params)
+        #binding.pry
+        @manifest = current_captain.ships.find_by_id(params[:id]).manifests.create(params)
         redirect '/ships/:id'
     end
 
--- app/models/manifest.rb
+++ app/models/manifest.rb
@@ -1,7 +1,4 @@
 class Manifest < ActiveRecord::Base
     belongs_to :ship
 
-    def self.valid_params?(params)
-        return !params[:item_name].empty? && !params[:item_quantity].empty?
-    end
 end
