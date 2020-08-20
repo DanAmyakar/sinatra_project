9e09e7fb9514681742e2d4d667da5730b5130365
DanAmyakar <dan.rockit@gmail.com>
2020-08-20 11:13:55 -0400

    WIP: Fixed new manifest method. Was using ship_id as manifest id, no longer does. Would not post back to ships page, now it does.

--- app/controllers/manifests_controller.rb
+++ app/controllers/manifests_controller.rb
@@ -3,6 +3,7 @@ class ManifestsController < ApplicationController
     # gets the form for a new manifest
     get '/manifests/new' do 
         redirect_if_not_signed_in
+        puts params
         @ship = current_captain.ships.find_by_id(params[:ship_id])
         #binding.pry
         erb :'manifests/new'
@@ -11,9 +12,15 @@ class ManifestsController < ApplicationController
     # creates a new manifest from form data and stores it in the ship
     post "/ships/:id" do
         redirect_if_not_signed_in
+        puts params
         #binding.pry
-        @manifest = current_captain.ships.find_by_id(params[:id]).manifests.create(params)
-        redirect '/ships/:id'
+        params[:ship_id] = params[:id]
+        @manifest = current_captain.ships.find_by_id(params[:ship_id]).manifests.new(item_name: params[:item_name], item_quantity: params[:item_quantity])
+        if @manifest.save
+            redirect "/ships/#{@manifest.ship.id}"
+        else
+            redirect "ships/#{current_captain.ships.find_by_id(params[:ship_id])}"
+        end
     end
 
     get "/manifests/:id/edit" do
